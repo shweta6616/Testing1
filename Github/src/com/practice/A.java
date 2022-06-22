b63d031c2aeb7efe1db0b5d72bae88f986cebe99
shweta6616 <104187437+shweta6616@users.noreply.github.com>
2022-06-22 11:37:08 +0530

    Update A.java

--- Github/src/com/practice/A.java
+++ Github/src/com/practice/A.java
@@ -1,9 +1,12 @@
 package com.practice;
 
 public class A {
-
+         int x;
 	public static void main(String[] args) {
 		System.out.println("hi");
+		A  a=new A();
+		  a.x=10;
+		System.out.println("variable "+a.x);
 
 	}
 
